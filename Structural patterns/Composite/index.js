853b2be5ecd0829dc61c5c40e206561cde6f0934
IPogorelova <pisseversk@mail.ru>
2020-05-27 17:47:04 +0300

    edit exercise code of the Composite pattern

--- Structural patterns/Composite/index.js
+++ Structural patterns/Composite/index.js
@@ -5,54 +5,25 @@ class SingleValue
     this.value = value;
   }
 
-  [Symbol.iterator]() {
+  [Symbol.iterator]()
+  {
     let returned = false;
     return {
       next: () => ({
-        value: this,
+        value: this.value,
         done: returned++
       })
     }
   }
 }
 
-class ManyValues
-{
-  // ensure there's a push(value) method
-  constructor() {
-    this.value = [];
-  }
-
-  push(number) {
-    this.value.push(number);
-  }
-}
+class ManyValues extends Array {}
 
 let sum = function(containers)
 {
-  let res = 0;
-  for (let item = 0; item < containers.length; item++) {
-    console.log('array length: ', containers[item].length)
-    if (!containers[item].length) {
-      console.log('containers[item].value: ', containers[item].value)
-      res += parseInt(containers[item].value);
-      console.log('res simple: ', res)
-    } else {
-      for (let number = 0; number < containers[item].value.length; number++) {
-        console.log('containers[item].value: ', parseInt(containers[item].value))
-        res += parseInt(containers[item].value[number]);
-      }
-      console.log('res complicated: ', res)
-    }
-  }
-
-  return res;
+  let result = 0;
+  for (let c of containers)
+    for (let i of c)
+      result += i;
+  return result;
 };
-
-let sv = new SingleValue(11);
-let mv = new ManyValues();
-
-mv.push(22);
-mv.push(33);
-
-console.log('sum: ', sum(sv, mv))
